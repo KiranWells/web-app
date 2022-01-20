b85860fd1df3152ea5506c05f5a92909865443ef
KiranWells <kiranwells1008@gmail.com>
2022-01-20 15:24:31 -0600

    Refactor custom render script

--- source/texme.js
+++ source/texme.js
@@ -1,29 +1,69 @@
-/* Texme.js from https://github.com/susam/texme/. @liscense MIT */
+// load texme
+var script = window.document.createElement('script')
+script.src = "https://cdn.jsdelivr.net/npm/texme"
+window.document.head.appendChild(script)
 
-!function () { "use strict"; var t, d = {}, m = {}; d.setDefaultOptions = function () { m.renderOnLoad = !0, m.useMathJax = !0, m.protectMath = !0, m.style = "viewer", m.onRenderPage = void 0, m.commonmarkURL = "https://cdn.jsdelivr.net/npm/commonmark@0.29.2/dist/commonmark.min.js", m.MathJaxURL = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js" }; d.setOption = function (e, n) { m[e] = n }; function e(e, n) { var t = window.document.createElement("script"); t.src = e, t.onload = n, window.document.head.appendChild(t) } d.tokenType = { MARK: 0, MASK: 1 }, d.tokenLiteral = { MASK: "::MASK::" }; var c = { plain: ["body {", "  color: #333;", "}", "main {", "  max-width: 40em;", "  margin-left: auto;", "  margin-right: auto;", "}", "h1, h2, h3, h4, h5, h6, h7 {", "  margin-top: 1.2em;", "  margin-bottom: 0.5em;", "}", "a:link, a:visited {", "  color: #03c;", "  text-decoration: none;", "}", "a:hover, a:active {", "  color: #03f;", "  text-decoration: underline;", "}", "img {", "  max-width: 100%;", "}", "pre, code, samp, kbd {", "  font-family: monospace,monospace;", "  font-size: 0.9em;", "  color: #009;", "}", "pre code, pre samp, pre kbd {", "  font-size: 1em;", "}", "pre {", "  padding: 0.5em;", "  overflow: auto;", "  background: #eee;", "}", "blockquote {", "  border-left: medium solid #ccc;", "  margin-left: 0;", "  margin-right: 0;", "  padding: 0.5em;", "  background: #eee;", "}", "blockquote *:first-child {", "  margin-top: 0;", "}", "blockquote *:last-child {", "  margin-bottom: 0;", "}"].join("\n"), viewer: ["@media screen and (min-width: 40em) {", "  body {", "    background: #444;", "  }", "  main {", "    background: #fff;", "    padding: 5em 6em;", "    margin: 1em auto;", "    box-shadow: 0.4em 0.4em 0.4em #222;", "  }", "}"].join("\n"), none: "" }; c.viewer = c.plain + c.viewer, d.tokenize = function (e) { for (var n, t, o = ["\\\\begin{(.*)}([\\s\\S]*?)\\\\end{\\1}", "\\\\\\[[\\s\\S]*?\\\\\\]", "\\\\\\([\\s\\S]*?\\\\\\)", "\\\\\\$", "\\$\\$(?:[^\\\\]|\\\\.)*?\\$\\$", "\\$(?:[^$\\\\]|\\\\.)+?\\$", d.tokenLiteral.MASK].join("|"), i = new RegExp(o, "g"), r = [], a = 0; null !== (n = i.exec(e));)n.index > a && (t = e.substring(a, n.index), r.push([d.tokenType.MARK, t])), void 0 !== n[1] && n[1].startsWith("md") ? r.push([d.tokenType.MARK, n[2]]) : r.push([d.tokenType.MASK, n[0]]), a = i.lastIndex; return t = e.substring(a), e.length > a && r.push([d.tokenType.MARK, t]), r }, d.mask = function (e) { for (var n, t, o = [], i = [], r = 0; r < e.length; r++)n = e[r][0], t = e[r][1], n === d.tokenType.MARK ? o.push(t) : (o.push(d.tokenLiteral.MASK), i.push(t)); return { text: o.join(""), tokenValues: i } }, d.unmask = function (e, n) { var t = new RegExp(d.tokenLiteral.MASK, "g"), o = 0; return e.replace(t, function () { return n[o++] }) }, d.renderCommonMark = function (e) { var n = (new t.Parser).parse(e); return (new t.HtmlRenderer).render(n) }, d.protectMathAndRenderCommonMark = function (e) { var n = d.tokenize(e), t = d.mask(n), o = d.renderCommonMark(t.text); return d.unmask(o, t.tokenValues) }, d.render = function (e) { return m.protectMath ? d.protectMathAndRenderCommonMark(e) : d.renderCommonMark(e) }, d.renderPage = function () { var e, n, t = window.document.getElementsByTagName("textarea"), o = window.document.createElement("main"); 0 < t.length ? (e = t[0].value.trim(), t[0].remove()) : (e = window.document.body.innerHTML.trim(), window.document.body.innerHTML = ""), void 0 !== window.document.title && "" !== window.document.title || (n = e.split("\n", 1)[0].replace(/^\s*#*\s*|\s*#*\s*$/g, ""), window.document.title = n), window.document.body.appendChild(o); var i = window.document.createElement("style"), r = c[m.style]; i.appendChild(window.document.createTextNode(r)), window.document.head.appendChild(i); var a = window.document.createElement("meta"); a.name = "viewport", a.content = "width=device-width; initial-scale=1.0", window.document.head.appendChild(a), o.innerHTML = d.render(e), m.useMathJax && window.MathJax.typeset(), void 0 !== m.onRenderPage && m.onRenderPage() }, d.main = function () { d.setDefaultOptions(), "undefined" != typeof window ? (function () { for (var e in m) "undefined" != typeof window && void 0 !== window.texme && void 0 !== window.texme[e] && (m[e] = window.texme[e]) }(), e(m.commonmarkURL, function () { t = window.commonmark }), m.useMathJax && (window.MathJax = { tex: { inlineMath: [["$", "$"], ["\\(", "\\)"]], tags: "ams" }, startup: { typeset: !1 } }, e(m.MathJaxURL)), m.renderOnLoad && (window.onload = d.renderPage), window.texme = d) : (t = require("commonmark"), module.exports = d) }, d.main() }();
+script.onload = () => {
+  let runTexme = window.onload || (() => { });
 
-// custom code for this site, mostly to add all-page content
-let runtexme = window.onload || (() => { });
-let currentTheme = "Dark";
-
-async function setCss() {
+// the following is a custom preprocessor and post-processor for texme,
+// allowing for a few extra features
+window.onload = () => {
   // set css
   currentTheme = localStorage.getItem("theme") || "Dark";
   document.body.parentElement.classList.add(currentTheme.toLowerCase());
-}
 
-function notify(text) {
-  const note = document.createElement('div');
-  note.classList.add("notify");
-  note.textContent = text;
-  document.body.appendChild(note);
-  setTimeout(() => { note.style.opacity = 0; setTimeout(() => document.body.removeChild(note), 1000) }, 1000);
+  // custom markdown pre-processing
+  let text = document.body.innerHTML;
+
+  // support for custom details element
+  text = replaceWithGlobal(text, /~~([^\n]+)\n(((?!~~).|\n)+)~~/m, match => {
+    let id_simplified = match[1].replace(/ /g, "-").toLowerCase();
+    return `
+
+<details id="${id_simplified}">
+  <summary>Click to show ${match[1]}</summary>
+
+${match[2]}
+
+</details>
+
+`});
+
+  // support for block quote element
+  text = replaceWithGlobal(text, /(^ *&gt;[^\n]+\n)+/m, match => `
+
+<blockquote>
+
+${match[0].replace(/^ *&gt;(.*)$/gm, "$1")}
+
+</blockquote>
+
+`)
+
+  // run TexMe and update final HTML
+  try {
+    // this should be sync, so this will not error inside of a promise
+    document.body.innerHTML = text;
+    runTexme();
+  } catch {
+    notify("Math rendering failed, retrying. This will only take a second...");
+    setTimeout(() => {
+      document.body.innerHTML = text;
+      runTexme();
+      afterTexMe();
+    }, 100);
+  } finally {
+    // run this anyway to make the page look right while waiting
+    afterTexMe();
+  }
 }
 
 function afterTexMe() {
   // add theme button
   let themes = ["Dark", "Light", "Tropical", "Summer", "Hyperlight", "Gruvbox", "Everforest"];
 
+  let currentTheme = "Dark";
   function changeTheme(name) {
     if (!themes.includes(name)) return -1;
     document.body.parentElement.classList.remove(currentTheme.toLowerCase());
@@ -34,32 +74,18 @@ function afterTexMe() {
 
   let themeToggle = document.createElement("div");
   themeToggle.classList.add("themetoggle")
-  let themeHTML = document.createElement("div");
-  themeHTML.classList.add("themelist");
+  let themeOptionsDiv = document.createElement("div");
+  themeOptionsDiv.classList.add("themelist");
   for (e of themes) {
-    let div = document.createElement("button");
-    div.innerHTML = e;
-    div.onclick = () => changeTheme(div.innerHTML);
-    div.classList.add("themeitem");
-    themeHTML.appendChild(div);
+    let option = document.createElement("button");
+    option.innerHTML = e;
+    option.onclick = () => changeTheme(option.innerHTML);
+    option.classList.add("themeitem");
+    themeOptionsDiv.appendChild(option);
   }
-  themeToggle.innerHTML = `
-  Theme
-  `;
-  // temporary dark/light toggle
-  // themeToggle.onclick = () => {
-  //   document.body.parentElement.classList.toggle("light");
-  //   currentTheme = currentTheme == "Light" ? "Dark":"Light";
-  //   themeToggle.innerHTML = currentTheme == "Light" ? "⚙": "☽";
-  //   localStorage.setItem("theme", currentTheme);
-  // }
-  themeToggle.appendChild(themeHTML);
+  themeToggle.innerHTML = "Theme";
+  themeToggle.appendChild(themeOptionsDiv);
   document.getElementsByTagName("main")[0].appendChild(themeToggle);
-
-  // add base href
-  var base = document.createElement("base");
-  base.href = window.location.href;
-  // document.head.appendChild(base);
 
   // add footer
   let footer = document.createElement("footer");
@@ -83,6 +109,8 @@ function afterTexMe() {
     I will remove it if necessary.
   </div>
     `;
+  document.body.firstChild.appendChild(footer);
+
   // add header ids
   let hs = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
   for (let h of hs) {
@@ -99,51 +127,42 @@ function afterTexMe() {
       notify(text);
     }
   }
-  document.body.firstChild.appendChild(footer);
 
   document.body.classList.add("loaded");
 }
 
-window.onload = () => {
-  setTimeout(setCss, 0);
-  let text = document.body.innerHTML;
-  // support for custom details element
-  text = text.replace(/~~([^\n]+)\n(((?!~~).|\n)+)~~/mg, `
-  <details id="__remove spaces__$1__end remove spaces__">
-    <summary>Click to show $1</summary>
-  
-$2
-  
-  </details>
-  
-  `);
+}
+
+/**
+ * Creates a notification div and displays it,
+ * then destroys it after 2 sec.
+ * @param {String} text - the message to display
+ */
+ function notify(text) {
+  const note = document.createElement('div');
+  note.classList.add("notify");
+  note.textContent = text;
+  document.body.appendChild(note);
+  setTimeout(() => { 
+    note.style.opacity = 0; 
+    setTimeout(() => document.body.removeChild(note), 1000);
+  }, 1000);
+}
+
+/**
+ * Replaces all matches (up to 10,000) of the regular expression
+ * in `text` with the result of calling `withFunc` on the match.
+ * @param {String} text - the text to replace in
+ * @param {RegEx} regex - the regular expression to replace
+ * @param {Function(Match) => String} withFunc - the function to use to modify the matched text
+ */
+function replaceWithGlobal(text, regex, withFunc) {
+  // limit to 10,000 matches
   for (let i = 0; i < 10000; i++) {
-    let match = text.match(/__remove spaces__([^_]*)__end remove spaces__/);
+    let match = text.match(regex);
     if (match === null) { break; }
-    let inner = match[1];
-    inner = inner.replace(/ /g, "-").toLowerCase();
-    text = text.replace(/__remove spaces__[^_]*__end remove spaces__/, inner);
+    let inner = withFunc(match);
+    text = text.replace(regex, inner);
   }
-  // support for block quote element
-  for (let i = 0; i < 10000; i++) {
-    let match = text.match(/(^ *&gt;[^\n]+\n)+/m);
-    if (match === null) { break; }
-    let inner = match[0];
-    inner = "\n\n<blockquote>\n\n" + inner.replace(/^ *&gt;/gm, "") + "\n\n</blockquote>\n\n";
-    text = text.replace(/(^ *&gt;[^\n]+\n)+/m, inner);
-  }
-  document.body.innerHTML = text;
-  // change to HTML
-  try {
-    // this should be sync, so this will not error in a promise
-    runtexme();
-  } catch {
-    notify("Math rendering failed, retrying. This will only take a second...");
-    setTimeout(() => {
-      document.body.innerHTML = text;
-      runtexme();
-      afterTexMe();
-    }, 1001);
-  }
-  afterTexMe();
+  return text;
 }
